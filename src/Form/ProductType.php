f3ba7a9feec769546e925473c8e25edcfa319be8
chfix <83886830+chfix@users.noreply.github.com>
2022-04-28 20:28:15 +0000

    /product created
     created: src/Controller/ProductController.php
     created: src/Form/ProductType.php
     created: templates/product/_delete_form.html.twig
     created: templates/product/_form.html.twig
     created: templates/product/edit.html.twig
     created: templates/product/index.html.twig
     created: templates/product/new.html.twig
     created: templates/product/show.html.twig

---
+++ src/Form/ProductType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('image')
+            ->add('active')
+            ->add('updatedAt')
+            ->add('createdAt')
+            ->add('Quantity')
+            ->add('category')
+            ->add('orders')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
